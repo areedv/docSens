e06f10e1f9f236f61c0f328dc08c1fc1a7e0ca2f
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-09-11 14:11:21 +0200

    fix style...

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-Rscript -e "rmarkdown::render_site(output_format = c('bookdown::html_book', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
